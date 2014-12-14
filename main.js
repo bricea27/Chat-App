1f919460aed6b16a116eeea94f4d6f2ae8a323c2
bricea27 <bricea27@gmail.com>
2014-12-14 12:07:50 -0500

    server update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Client File
-var client = new WebSocket("ws://andrew.princesspeach.nyc:3000");
+var client = new WebSocket("ws://localhost:3000");
 
 client.addEventListener("open", function() {
   console.log('connected');
@@ -7,36 +7,31 @@ client.addEventListener("open", function() {
   //HTML elements
   var body = document.querySelector("body");
   var ul = document.querySelector("ul");
-  var button = document.getElementById("button");
-  var input = document.getElementById("input");
   var userName = document.getElementById("username");
   var userInput = document.getElementById("userinput");
+  var button = document.getElementById("button");
+  var input = document.getElementById("input");
+  var display = document.getElementById("display");
 
   button.addEventListener("click", function(){
+
     //create messageObject with name and message
     var messageObject = {name: "Anonymous:", msg: input.value};
     //take messageObject, stringify and send to server
 
     //will only send something if the inputs actually have text
     if ((userName.value.trim() != "") && (input.value.trim() != "")) {
-      messageObject.name = userName.value + ":";
-      //resets input box
-      input.value = "";
+      messageObject.name = userName.value + ": ";
+      //resets input boxes
       userInput.style.display = "none";
       userName.style.display = "none";
+      input.value = "";
       client.send(JSON.stringify(messageObject));
     }
-
   });//end button click function
 
   // on pressing enter
   input.addEventListener("keypress", function(){
-    if(event.keyCode === 13){
-      button.click();
-    }
-  });
-
-  userName.addEventListener("keypress", function(){
     if(event.keyCode === 13){
       button.click();
     }
@@ -52,9 +47,18 @@ client.addEventListener("open", function() {
     var ul = document.querySelector("ul");
     var li = document.createElement("li");
 
-    li.innerText = newMessage.name + " " + newMessage.msg;
+    var nameSpan = document.createElement("span");
+    var msgSpan = document.createElement("span");
+    nameSpan.innerText = newMessage.name;
+    msgSpan.innerText = newMessage.msg;
+
+    li.appendChild(nameSpan);
+    li.appendChild(msgSpan);
     //this will put the list element at the top of the list
-    ul.insertBefore(li, ul.firstChild);
+    ul.appendChild(li);
+
+    display.scrollTop = display.scrollHeight;
+
   });
 
 
